324734745e4a563bdd8b7552fb2e379e80001385
bach <bachlynn23@gmail.com>
2024-12-19 05:14:13 -0500

    assignment

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,38 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+// Immutable variable for the resolution
+const resolution = "Learn a new programming language";
+
+// Immutable variable for the current year
+const currentYear = 2025;
+
+// Mutable variable to track whether the resolution will be met
+let willMeetResolution = true;
 
 function updateYear() {
     // Update this function
-
+    yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
     // Update this function 
-
+    resolutionElement.innerText = resolution;
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+    willMeetResolutionElement.innerText = willMeetResolution
 }
 
 function render() {
     // Update this function
-
+    updatecurrentYearSentence();
+    updateResolutionSentence();
+    updatewillMeetResolutionSentence();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render();
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@
 // that are not declared
 "use strict"
 
+/*
+let isStudent = true;
+console.log(isStudent);           // Output: true
+console.log(typeof isStudent);    // Output: "boolean"
+
+const myName = "Rebecca";
+console.log(myName)               // Output: "Rebecca"
+console.log(typeof myName)        // Output: "string"
+
+const pi = 3.14159;
+console.log(pi)                   // Output: 3.14159
+console.log(typeof pi);           // Output: "number"
+*/
